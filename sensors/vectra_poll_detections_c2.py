d5333889a0dcf9fccecc10bff94f1929e4c1e20b
Pavel <radar.fami@gmail.com>
2019-12-15 21:02:16 +0300

    typo fix 2

--- sensors/vectra_poll_detections_c2.py
+++ sensors/vectra_poll_detections_c2.py
@@ -12,7 +12,7 @@ headers = {'Content-Type': 'application/json', 'Authorization': TOKEN}
 
 class VectraPollDetectionsC2(Sensor):
     def __init__(self, sensor_service, config):
-        super(VectraPolldetectionsC2, self).__init__(
+        super(VectraPollDetectionsC2, self).__init__(
             sensor_service=sensor_service, config=config)
         self._logger = self.sensor_service.get_logger(
             name=self.__class__.__name__)
